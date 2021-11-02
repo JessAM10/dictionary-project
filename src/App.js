abc6df396c7cda853bc6783e4b2268292fc7230f
JessAM10 <87210462+JessAM10@users.noreply.github.com>
2021-11-02 14:59:46 -0400

    Added footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Dictionary />
+        <footer>
+          <a
+            href="https://github.com/JessAM10/dictionary-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Coded
+          </a>{" "}
+          by Jessica Allard
+        </footer>
       </div>
     </div>
   );
