7b21c4acea14c2e01de036f2eaa754180bff02d6
JessAM10 <87210462+JessAM10@users.noreply.github.com>
2021-11-02 11:39:40 -0400

    Removed an incomplete function

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,20 +4,13 @@ import "./Dictionary.css";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
-  const [definition, setDefinition] = useState({ ready: false });
 
   function handleResponse(response) {
     console.log(response.data[0]);
-    setDefinition({
-      ready: true,
-      meaning: response.data.meanings[0],
-
-    })
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
